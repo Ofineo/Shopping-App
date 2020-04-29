9e42e689675a891e786e6ea660f4a5a4424e641e
Jordi <ofineo@gmail.com>
2020-04-29 19:56:32 +0100

    setup navigation for productOverview and productDetail

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -1,11 +1,34 @@
 import React from "react";
+import { Platform } from "react-native";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import ProductOverviewScreen from "../screens/ProductOverviewScreen";
+import ProductDetailScreen from "../screens/ProductDetailScreen";
+import Colors from "../constants/Colors";
 
-const ShopNavigator = createStackNavigator({
-  Shop: { screen: ProductOverviewScreen },
+const ProductNavigator = createStackNavigator(
+  {
+    ProductsOverview: { screen: ProductOverviewScreen },
+    ProductDetail:{screen: ProductDetailScreen},
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: Platform.OS === "android" ? Colors.primary : "",
+      },
+      headerTintColor: Platform.OS === "android" ? "white" : Colors.primary,
+      headerTitleStyle: {
+        fontFamily: 'bb2-bold'
+      },
+      headerBackTitleStyle:{
+          fontFamily: 'bb2'
+      }
+    },
+  }
+);
 
-});
+ProductOverviewScreen.navigationOptions ={
+    title: 'All products'
+}
 
-export default createAppContainer(ShopNavigator);
+export default createAppContainer(ProductNavigator);
