9aa4214f88d90a70d36beddb368d8ee74101bd4c
Jordi <ofineo@gmail.com>
2020-05-05 21:05:31 +0100

    implemented auto login

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -12,6 +12,7 @@ import { Ionicons } from "@expo/vector-icons";
 import UserProductScreen from "../screens/UserProductsScreen";
 import EditProductScreen from "../screens/EditProductScreen";
 import AuthScreen from "../screens/AuthScreen";
+import StartupScreen from "../screens/StartupScreen";
 
 const defaultNav = {
   headerStyle: {
@@ -105,6 +106,7 @@ const AuthNavigator = createStackNavigator(
 );
 
 const MainNavigator = createSwitchNavigator({
+  Startup: StartupScreen,
   Auth: AuthNavigator,
   Shop: ShopNavigator,
 });
--- store/actions/auth.js
+++ store/actions/auth.js
@@ -1,5 +1,11 @@
-export const SIGN_UP = "SIGN_UP";
-export const LOGIN = "LOGIN";
+import { AsyncStorage } from "react-native";
+// export const SIGN_UP = "SIGN_UP";
+// export const LOGIN = "LOGIN";
+export const AUTHENTICATE = "AUTHENTICATE";
+
+export const authenticate = (userId, token) => {
+  return { type: AUTHENTICATE, token: token, userId: userId };
+};
 
 export const signUp = (email, password) => {
   return async (dispatch) => {
@@ -34,11 +40,12 @@ export const signUp = (email, password) => {
 
     const resData = await response.json();
 
-    dispatch({
-      type: SIGN_UP,
-      token: resData.idToken,
-      userId: resData.localId,
-    });
+    dispatch(authenticate(resData.localId, resData.idToken));
+    //save to internal storage
+    const expirationDate = new Date(
+      new Date().getTime() + +resData.expiresIn * 1000
+    );
+    saveDataToStorage(resData.idToken, resData.localId, expirationDate);
   };
 };
 export const login = (email, password) => {
@@ -77,6 +84,21 @@ export const login = (email, password) => {
 
     const resData = await response.json();
     console.log(resData);
-    dispatch({ type: LOGIN, token: resData.idToken, userId: resData.localId });
+    dispatch(authenticate(resData.localId, resData.idToken));
+    const expirationDate = new Date(
+      new Date().getTime() + +resData.expiresIn * 1000
+    );
+    saveDataToStorage(resData.idToken, resData.localId, expirationDate);
   };
+};
+
+const saveDataToStorage = (token, userId, expirationDate) => {
+  AsyncStorage.setItem(
+    "userData",
+    JSON.stringify({
+      token: token,
+      userId: userId,
+      expiryDate: expirationDate.toISOString(),
+    })
+  );
 };
